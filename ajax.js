cb33a611503eab155760029cebceda6465c9a95b
xujj <939160741@qq.com>
2019-01-21 17:21:05 +0800

    接受两个参数和headers

--- ajax.js
+++ ajax.js
@@ -6,14 +6,25 @@ window.jQuery = function (nodeOrSelector) {
 }
 
 window.jQuery.ajax = function (options) {
-  let url = options.url
+  let url
+  if (arguments.length === 1) {
+    url = options.url
+  } else if (arguments.length === 2) {
+    url = arguments[0]
+    options = arguments[1]
+  }
   let method = options.method
   let body = options.body
   let successFn = options.successFn
   let failFn = options.failFn
+  let headers = options.headers
 
   let request = new XMLHttpRequest()
   request.open(method, url)
+  for (let key in headers) {
+    let value = headers[key]
+    request.setRequestHeader(key, value)
+  }
   request.onreadystatechange = () => {
     if (request.readyState === 4) {
       if (request.status >= 200 && request.status < 300) {
@@ -35,6 +46,10 @@ myButton.addEventListener('click', (e) => {
   window.jQuery.ajax({
     url: '/xxx',
     method: 'get',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'frank': '18'
+    },
     successFn: (x) => {
       f1.call(undefined, x)
       f2.call(undefined, x)
