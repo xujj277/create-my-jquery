6fb0d25334c59b6ee23b15c4f3584659d3c9768b
xujj <939160741@qq.com>
2019-01-21 23:23:33 +0800

    new promise

--- ajax.js
+++ ajax.js
@@ -5,46 +5,49 @@ window.jQuery = function (nodeOrSelector) {
   return nodes
 }
 
-window.jQuery.ajax = function ({url, method, body, successFn, failFn, headers}) {
-  // ES6 解构赋值
-  let request = new XMLHttpRequest()
-  request.open(method, url)
-  for (let key in headers) {
-    let value = headers[key]
-    request.setRequestHeader(key, value)
+window.Promise = function (fn) {
+  // ..
+  return {
+    then: function () {}
   }
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status >= 200 && request.status < 300) {
-        successFn.call(undefined, request.responseText)
-      } else if (request.status >= 400) {
-        failFn.call(undefined, request)
-      }
-    }
-  }
-  request.send(body)
 }
 
-function f1 (responseText) {}
-function f2 (responseText) {}
+window.jQuery.ajax = function ({url, method, body, headers}) {
+  return new Promise(function (resolve, reject) {
+    // ES6 解构赋值
+    let request = new XMLHttpRequest()
+    request.open(method, url)
+    for (let key in headers) {
+      let value = headers[key]
+      request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if (request.status >= 200 && request.status < 300) {
+          resolve.call(undefined, request.responseText)
+        } else if (request.status >= 400) {
+          reject.call(undefined, request)
+        }
+      }
+    }
+    request.send(body)
+  })
+}
 
 window.$ = window.jQuery
 
 myButton.addEventListener('click', (e) => {
-  window.jQuery.ajax({
+  let promise = window.jQuery.ajax({
     url: '/xxx',
     method: 'get',
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
       'frank': '18'
-    },
-    successFn: (x) => {
-      f1.call(undefined, x)
-      f2.call(undefined, x)
-    }, // callback 就是函数
-    failFn: (x) => {
-      console.log(x)
-      console.log(x.statusText)
     }
   })
+
+  promise.then(
+    (text) => {console.log(text)},
+    (request) => {console.log(request)}
+  )
 })
